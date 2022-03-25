be0d064129542e71ed5d070bafffde7aa49bbe0d
ayushkayshab04 <ayushkayshab04@gmail.com>
2022-03-25 15:52:58 +0530

    Last one

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
-const app = require("express")()
+const express = require("express")
 const path = require("path")
 const shortid =  require("shortid")
 const Razorpay = require("razorpay")
 const cors = require("cors")
+const crypto = require("crypto")
 
+const app = express()
+app.use(express.json())
 app.use(cors())
 
 const razorpay = new Razorpay({
@@ -12,9 +15,38 @@ key_secret:"RmSSZfB2H3tpPUS9Lr8F9MnO"
 })
 
 
+
 app.get("/logo.svg",(req,res)=>{
     res.sendFile(path.join(__dirname,"logo.svg"))
 })
+
+
+app.post("/verification",(req,res)=>{
+    const SECRET = "12345678"
+    
+    console.log(req.body)
+
+
+
+    const shasum = crypto.createHmac("sha256",SECRET)
+    shasum.update(JSON.stringify(req.body))
+    const digest = shasum.digest("hex")
+
+    console.log(digest, req.headers["x-razorpay-signature"])
+    if(digest === req.headers["x-razorpay-signature"])
+    console.log("Request is legit")
+
+
+    res.json({status:"ok"})
+})
+
+app.post("/failed",(req,res)=>{
+const SECRET = "037257"
+console.log(req.body)
+res.json({status:"Payment Failed"})
+})
+
+
 
 app.post("/razorpay" ,async(req,res)=>{
     const  payment_capture = 1
